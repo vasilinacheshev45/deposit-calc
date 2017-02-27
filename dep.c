27fade0c6cbd18b0c7d546ab2bf59bd6ba687b46
Чёшева Василина Сергеевна <ri68s18@mwp08.csc.local>
2017-02-27 14:50:28 +0700

    vvod

---
+++ dep.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int day, sum, dox;
+    printf("Vvedite summu:");
+    scanf("%d", &sum);
+    printf("Vvedite dey");
+    scanf("%d", &day);
+
